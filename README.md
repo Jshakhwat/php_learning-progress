f1b9a90a876ea70b41e8b68838574afd90a643f8
Jshakhwat <99986085+Jshakhwat@users.noreply.github.com>
2023-12-18 20:36:07 +0600

    added more practice.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # php_learning-progress
+# the project is all about learning
--- day_1.php
+++ day_1.php
@@ -8,7 +8,7 @@
 <body>
     <h1> <?php echo "Hello, PHP!"; ?> </h1>
     <hr>
-    <h2><?php echo "This is the frist attempt to learn PHP"; ?></h2>
+    <h2><?php echo "This is the frist attempt to learn PHP!"; ?></h2>
     <!-- i am writing my name -->
     <p>
         <?php
@@ -54,53 +54,190 @@
             // $secondName = "Hossain";
             // echo $firstName." ".$secondName
 
-        $isPassed = true;
-        if ($isPassed){
-            echo "you have passed the exam"."<br>";
-        }
-        else{
-            echo"fail"  ;     
-         }
-         $isPassed = true;
-        if (!$isPassed){
-            echo "you have failed the exam"."<br>";
-        }
-        else{
-            echo"Pass"."<br>";     
-         }
+        // $isPassed = true;
+        // if ($isPassed){
+        //     echo "you have passed the exam"."<br>";
+        // }
+        // else{
+        //     echo"fail"  ;     
+        //  }
+        //  $isPassed = true;
+        // if (!$isPassed){
+        //     echo "you have failed the exam"."<br>";
+        // }
+        // else{
+        //     echo"Pass"."<br>";     
+        //  }
 
 /////////////////////////////////
-        $age =10;
-        $status = ($age >= 18) ? "Adult":"Minor";
-        echo "$status"."<br>";
+        // $age =10;
+        // $status = ($age >= 18) ? "Adult":"Minor";
+        // echo "$status"."<br>";
 ////////////////////////////////////  
-        $num1 = 20;
-        $num2 = 26;
-        $status = ($num1 > $num2) ? "num1 is bigger":"num2 is bigger";
-        echo "$status"."<br>";
+//         $num1 = 20;
+//         $num2 = 26;
+//         $status = ($num1 > $num2) ? "num1 is bigger":"num2 is bigger";
+//         echo "$status"."<br>";
 
 
 
-$value = 20;
-if ($value < 10){
-    echo"The value is less than 10";
-}
-elseif($value < 20){
-    echo "The value is under 20";
-}     
- else{
-    echo "The value is more than 20"."<br>";
- }
- $temperature = 25;
- $status = ($temperature >= 25) ? "Hot":"Cold";
- echo "$status"."<br>"; 
+// $value = 20;
+// if ($value < 10){
+//     echo"The value is less than 10";
+// }
+// elseif($value < 20){
+//     echo "The value is under 20";
+// }     
+//  else{
+//     echo "The value is more than 20"."<br>";
+//  }
+//  $temperature = 25;
+//  $status = ($temperature >= 25) ? "Hot":"Cold";
+//  echo "$status"."<br>"; 
 
 ///////turnery operator//////////
- $temp = 24;
- $sta = ($temp > 25)? "It's a hot day !":"It's a cold day !";
- echo "$sta"
+//  $temp = 24;
+//  $sta = ($temp > 25)? "It's a hot day !":"It's a cold day !";
+//  echo "$sta";
 
-   ?>
+/////////nested if///////
+// $result = 98;
+// if ($result >= 90){
+//     echo "A";}
+// elseif ($result >=80){
+//     echo "B";
+//     if ($result >= 85){
+//         echo"+";
+//     }
+// }
+// else {
+//     echo "C";
+// }
+
+/////////switch case////////
+// $day = "Monday";
+
+// switch($day){
+//     case "Sunday";
+//     echo "It's a Sunday";
+//     break;
+//     case "Monday";
+//     echo "It's a Monday";
+//     break;
+//     default:
+//     echo "It's a regular day";
+// }
+
+// $grade = "h";
+// switch($grade){
+//     case "A":
+//     case "B":
+//     case "C":
+//     case "D":
+//         echo "Pass";
+//         break;
+//     case "E":
+//     case "F":
+//         echo "Fail";
+//         break;
+//     default:
+//     echo "invalid grade";
+            
+    
+    
+// }
+////////problem2////////
+    
+// $value = 8;
+// if ($value > 0){
+//     echo"The value is positive";
+// }
+// elseif($value < 0){
+//     echo "The value is negetive";
+// }     
+// else{
+//     echo "The value is zero"."<br>";
+//  }
+/////////problem3///////////////////
+
+
+// $value = 56;
+// if ($value > 0 && $value < 12){
+//     echo"The person is child"."<br>";
+// }
+// elseif($value < 19 && $value > 13){
+//     echo "The person is teeneger"."<br>";
+// }    
+
+// elseif($value > 19 && $value < 69){
+//     echo "The person is adult"."<br>";
+// }  
+// else{
+//     echo "The person is old"."<br>";
+//  }
+
+///////looops//////////////
+//for looop/////
+// for ($i = 1; $i < 21; $i++){
+//     echo $i."<br>";
+// }
+
+
+// for ($i = 1; $i < 21; $i+=2){
+//     echo $i."<br>";
+// }
+
+////while loop///////
+// $counter = 1;
+// while($counter < 21){
+//     echo $counter."<br>";
+//     $counter++;
+// }
+
+//////do while////////
+// $counter = 1;
+// do{
+//     echo $counter."<br>";
+//     $counter+=2;
+// }
+// while($counter < 21);
+///////foreach loop//////
+//1st///
+// $data = array(1,2,3,4,5,6,7);
+// foreach ($data as $value){
+//     echo "$value"."<br>";
+// }
+
+//2nd//
+// $data = array("johnny","ronty",3,4,5,6,7);
+// foreach ($data as $value){
+//     echo "$value".","."<br>";
+// }
+//3rd////
+// $data = array("johnny","ronty",3,4,5,6,7);
+// foreach ($data as $d=>$value){
+//     echo $d.".".$value."<br>";
+// }
+///4rth/////
+// $data = array("johnny","ronty",3,4,5,6,7);
+// foreach ($data as $d=>$value){
+//     echo ++$d.".".$value."<br>";
+// }
+
+
+//home-work////
+// problem2
+
+
+for ($i = 1; $i < 10; $i++){
+    echo $i."<br>";
+}
+for ($i = 10; $i > 0; $i--){
+    echo $i."<br>";
+}
+
+
+ ?>
 
 </p>
 
