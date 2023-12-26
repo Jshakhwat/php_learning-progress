cdaab2e6dd814bd270ce21e6cff0ea3f8cf60658
Jshakhwat <99986085+Jshakhwat@users.noreply.github.com>
2023-12-26 19:29:48 +0600

    2nd class added

---
+++ class_2.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <P>
+<?php
+//.........time zone..................
+// date_default_timezone_set("Asia/Dhaka");
+// echo "current time:".date("y.m.h:i:sa");
+
+//.......string lenth.................
+// echo strlen("Hello Bangladesh");
+
+//.......to convert lowe case...........
+// echo strtolower("I am A boy");
+
+//........to convert upper case..............
+// echo strtoupper("hey what's up?");
+//........to remove string.............
+//echo substr ("hello world",6);
+
+//......to replace string .............
+//echo str_replace("firstName", "Johnny","Mr.firstName");
+
+//......to make round figure...after .5 means next number and less than 5 is round..
+//echo round(3.6);
+
+//....random number in between 10 to 20...........
+//echo rand (10,20);
+
+//....to read file...............
+// echo readfile("text.txt");
+
+//....to open file.................
+// $file = fopen('text.txt', 'r');
+// $readfile = fgets($file);
+// echo $readfile
+
+//......to write a file.........................
+// $file = open("text.txt", "w");
+// $txt = "There is will there is hope\n";
+// fwrite($file,$txt);
+// $txt = "sir\n";
+// fwrite($file,$txt);
+// fclose($file);
+// echo readfile("text.txt");
+
+
+//......to use only absulate number...........
+//echo abs (-2);
+//echo abs (-8.3);
+
+//to start the session..................
+// session_start();
+
+//set session variables...........
+// $_SESSION["favcolor"] = "green";
+// $_SESSION["favanimal"] = "cat";
+// echo "Session Variable are set.";
+
+
+?>
+
+
+
+    </P>
+</body>
+</html>
